d6772e3731666bd83092e6cd9d9329860dffae79
nsalminen <n.salminen@me.com>
2019-11-14 13:58:11 +0100

    Restructure content and styles

--- public/scripts.js
+++ public/scripts.js
@@ -1,18 +1,22 @@
-
-
-
 var scene = new THREE.Scene();
 
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 70;
-camera.zoom = 100;
+camera.zoom = 1;
 
-var renderer = new THREE.WebGLRenderer({ alpha: true });
-renderer.setSize( window.innerWidth/1.5, window.innerHeight/1.5 );
+var renderer = new THREE.WebGLRenderer({alpha: true});
 
-document.getElementById('container').appendChild( renderer.domElement );
+var container = document.getElementById('obj-container');
+container.appendChild(renderer.domElement);
 
+window.addEventListener( 'resize', onWindowResize, false );
 
+function onWindowResize(){
+    camera.aspect = container.clientWidth / container.clientHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize( container.clientWidth, container.clientHeight );
+}
+onWindowResize();
 
 var controls = new THREE.OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
@@ -20,7 +24,6 @@ controls.dampingFactor = 0.25;
 controls.enableZoom = false;
 controls.autoRotate = true;
 controls.autoRotateSpeed = 0.5;
-
 
 var keyLight = new THREE.DirectionalLight(new THREE.Color('hsl(360, 100%, 100%)'), 0.8);
 keyLight.position.set(-100, 0, 100);
@@ -35,40 +38,17 @@ scene.add(keyLight);
 scene.add(fillLight);
 scene.add(backLight);
 
-// var mtlLoader = new THREE.MTLLoader();
-// mtlLoader.setTexturePath('/examples/3d-obj-loader/assets/');
-// mtlLoader.setPath('/examples/3d-obj-loader/assets/');
-// mtlLoader.load('r2-d2.mtl', function (materials) {
-
-//     materials.preload();
-
-    var objLoader = new THREE.OBJLoader();
-    // objLoader.setMaterials(materials);
-    objLoader.setPath('assets/');
-    objLoader.load('bottlecapeyewasher.obj', function (object) {
-
-        scene.add(object);
-        object.position.y -= 40;
-
-    });
-
-// });
+var objLoader = new THREE.OBJLoader();
+objLoader.setPath('assets/');
+objLoader.load('bottlecapeyewasher.obj', function (object) {
+    scene.add(object);
+    object.position.y -= 35;
+});
 
 var animate = function () {
-	requestAnimationFrame( animate );
-	controls.update();
-	renderer.render(scene, camera);
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
 };
 
 animate();
-
-
-
-window.onload = window.onresize = function() {
-    var canvas = document.getElementById('test');
-    canvas.width = window.innerWidth * 0.8;
-    canvas.height = window.innerHeight * 0.8;
-
-
-}
-
